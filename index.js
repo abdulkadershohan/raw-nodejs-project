3997c46efbd65caa8cd336a9c940dcf36cf1ac0f
Abdul Kader <abdulkadirshohan@gmail.com>
2021-11-17 23:39:54 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // dependencies
 const http = require('http');
-
+const url = require('url');
+const { StringDecoder } = require('string_decoder');
 // app object - module scaffolding
 app = {};
 
@@ -19,8 +20,29 @@ app.createServer = () => {
 
 // handel requst responce
 app.handelReqResponce = (req, res) => {
-    // responce handel
-    res.end('hello world');
+    // requrest handel
+
+    // get the url and parse it
+    const parseUrl = url.parse(req.url, true);
+    const path = parseUrl.pathname;
+    const trimendPath = path.replace(/^\/+|\/+$/g, '');
+    const method = req.method.toLowerCase();
+    const queryStringObject = parseUrl.query;
+    const reqHeadersObject = req.headers;
+
+    const decoder = new StringDecoder('utf-8');
+    let realData = '';
+
+    req.on('data', (buffer) => {
+        realData += decoder.write(buffer);
+    });
+    req.on('end', () => {
+        realData += decoder.end();
+        console.log(realData);
+        console.log(method);
+        // responce handel
+        res.end('hello world');
+    });
 };
 
 // start the server
